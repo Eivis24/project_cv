5d1dad1aa4e607e30e21e23b40e0b15444df906e
Eivydas Petryla <tyrasvyras@gmail.com>
2017-11-20 09:20:34 +0200

    finished the general footer (but there are still positioning problems), header is done (clock positioning problems), center div is missing the right border, social bar(center div) is in the wrong place, still missing hex style, bootstrap columns, other page editing and style and misc i forgot.

--- page.php
+++ page.php
@@ -93,8 +93,8 @@
 				<hr>
 				<div class="helper1">
 					<div class="about col-lg-12">
-						<img class="profile col-lg-2" src="#" alt="profile foto">
-						<span class="about-by-img col-lg-7">
+						<img class="profile col-lg-12" src="#" alt="profile foto">
+						<span class="about-by-img col-lg-7 col-md-7 col-sm-12">
 							<section>
 								<h1>
 									<!-- Eivydas Petryla -->
@@ -141,44 +141,46 @@
 					<div class="contact">
 						<div class="single">
 							<div class="txt-bold">
-								Get In Touch
-							<!-- Susisiekime -->
+								<!-- Get In Touch -->
+							Susisiekime
 							</div>
 							<div class="txt-semiBold">
-								In order to get in touch use the contact form below:
-								<!-- Norėdami su manimi susiekti užpildykite žemiau nurodytus duomenis. -->
+								<!-- In order to get in touch use the contact form below: -->
+								Norėdami su manimi susiekti užpildykite žemiau nurodytus duomenis:
 							</div>
 						</div>
 						<div class="structure-helper">
-						<form>
-							<input type="text" name="name" placeholder="Jūsų vardas">
-							<input type="text" name="email" placeholder="Jūsų elektroninio pašto adresas">
-							<input type="text" name="subject" placeholder="Tema">
-							<textarea rows="6" name="content" placeholder="Jūsų žinutė"></textarea>
-							<button type="submit" class="btn">Siųsti</button>
-						</form>
-						<div id="text">
-							<div>
-								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-								tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-								quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-								consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-								cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-								proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Einam alaus?
+							<form class"col-lg-8 col-md-8 col-sm-12">
+								<input type="text" name="name" placeholder="Jūsų vardas">
+								<input type="text" name="email" placeholder="Jūsų elektroninio pašto adresas">
+								<input type="text" name="subject" placeholder="Tema">
+								<textarea rows="6" name="content" placeholder="Jūsų žinutė"></textarea>
+								<button type="submit" class="btn">Siųsti</button>
+							</form>
+							<div class="text col-lg-3 col-md-3 col-sm-12">
+								<div>
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+									tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+									quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+									consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+									cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+									proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Einam alaus?
+								</div>
+								<div class="instaTac">
+									<a id="phone" href="tel:+37065555555">
+										<i class="fa fa-phone" aria-hidden="true"></i>
+										<span>+370 65 555 555</span>
+									</a>
+									<a id="email" href="mailto:epastas@tekejas.lt">
+										<i class="fa fa-envelope" aria-hidden="true"></i>
+										<span>epastas@tekejas.lt</span>
+									</a>
+								</div>
 							</div>
-							<div>
-								<a id="phone" href="tel:+37065555555">
-									<i class="fa fa-phone" aria-hidden="true">+37065555555</i>
-								</a>
-								<a id="email" href="mailto:epastas@tekejas.lt">
-									<i class="fa fa-envelope" aria-hidden="true">epastas@tekejas.lt</i>
-								</a>
-							</div>
-						</div>
 						</div>
 					</div>
 				</div>
-				<div class="footer-bottom col-lg-12">
+				<div class="footer-bottom col-lg-12 col-md-8 col-sm-12">
 					Nevok mano dizaino ir mes sutarsim ;)
 				</div>
 			</footer>
